622f7b4680814bd2e658b677526ef2254e5d8130
abhaysodha <abhaysodha6123@gmail.com>
2026-05-11 23:33:08 +0530

    testing http methods...

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/dashboard",(req,res) =>{
-    res.send("banavo dashboard abhay bhai");
+//This will only handle GET call to /user
+app.get("/user",(req,res) => {
+    res.send({firstname : "abhay" ,lastname :"sodha"});
 });
 
-app.use((req,res) => {
-    res.send("hello from the server to abhay sodha");
+//This will only handle GET call to /user
+app.post("/user",(req,res) =>{
+
+    //Here we wil writeing logic to store data in db
+
+    res.send("Data successfully saved on database");
+})
+
+app.delete(("/user"),(req,res) =>{
+    res.send("Deleted user successfully!");
 });
 
 app.listen(9999, () =>{
