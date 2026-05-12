b94315b0ee83ecd875ad9c7df566935bd67ac5cb
abhaysodha <abhaysodha6123@gmail.com>
2026-05-13 01:37:55 +0530

    samll modles methods learning

--- src/app.js
+++ src/app.js
@@ -19,6 +19,36 @@ app.post("/signup",async (req,res) =>{
 });
 
 
+app.get("/user",async (req,res) => {
+
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId : userEmail});
+        if(users.length === 0){
+            res.status(404).send("user not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch (err){
+        res.status(400).send("somthing went wrong");
+    }
+});
+
+app.get("/feed", async (req,res) =>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+
+
+//feed api - GET /feed -get all the user from the database  
 connectDB()
 .then(()=>{
     console.log("Database connected successfully");
